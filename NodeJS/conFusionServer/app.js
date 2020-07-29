463685c7eaeeb0b5ccc61b83b5a20b4f1c1eee50
ishaanbassi <ishaan16238@iiitd.ac.in>
2020-07-29 18:53:04 +0530

    Express REST API with MongoDB and Mongoose Part 1

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -4,17 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); })
+
+
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-
-
 
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
