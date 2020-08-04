83173a7dc27bc8a5fbab6ea25e9bd40d8b79997a
ishaanbassi <ishaan16238@iiitd.ac.in>
2020-08-04 17:45:34 +0530

    Express sessions part 2

--- NodeJS/conFusionServer/app.js
+++ NodeJS/conFusionServer/app.js
@@ -24,12 +24,53 @@ var leaderRouter = require('./routes/leaderRouter');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
+
+
+
 var app = express();
 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+
+app.use(session({
+  name: 'session-id',
+  secret: '12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+//client has to be first authorized before static data is available
+function auth (req, res, next) {
+    console.log(req.session);
+
+  if(!req.session.user) {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+  }
+  else {
+    if (req.session.user === 'authenticated') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
+    }
+  }
+} 
+
+app.use(auth);
 
 app.use('/dishes',dishRouter);
 app.use('/promotions',promoRouter);
@@ -38,16 +79,21 @@ app.use('/leaders',leaderRouter);
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser('12345-67890-09876-54321'));
+
+
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
